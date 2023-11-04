92e823c8cc4b52509a215d30159b58799d6cf248
josecarl0s1 <72897182+josecarl0s1@users.noreply.github.com>
2023-11-04 00:57:21 -0600

    BulletWorks!

---
+++ Assets/Scripts/ShootingController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingController : MonoBehaviour
+{
+    public float timeSinceLastFire = 0.0f;
+
+    public float fireRate = 5f;
+    public float bulletSpeed = 5.0f;
+    public GameObject bullet;
+
+    public Camera mainCamera;
+    void Start(){
+        mainCamera = Camera.main;
+    }
+    void Update(){
+        if(Input.GetMouseButton(0)){
+            Shoot();
+        }
+    }
+    private void Shoot(){
+        if ((Time.time - timeSinceLastFire) >= (1f/fireRate)) {
+            // Obtener dirección de disparo
+            Vector3 worldMousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 direction = worldMousePos - transform.position;
+            direction.Normalize();
+            GameObject newBullet;            
+            newBullet = Instantiate(bullet, transform.position, transform.rotation);
+            /*clone.GetComponent<PlayerBullet>().bulletDamage = bulletDamage;
+            clone.GetComponent<PlayerBullet>().bulletSpeed = bulletSpeed;
+            clone.GetComponent<PlayerBullet>().bulletDirection = direction;*/
+            newBullet.GetComponent<Rigidbody2D>().velocity = (direction) * (bulletSpeed);
+
+
+            // Actualizar
+            timeSinceLastFire = Time.time;
+        }
+    }
+}
