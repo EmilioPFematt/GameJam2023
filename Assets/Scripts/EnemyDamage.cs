8e63ebcd9a562cedf1e29011d9bcface5690097e
josecarl0s1 <72897182+josecarl0s1@users.noreply.github.com>
2023-11-04 04:46:40 -0600

    Small Changes

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -8,23 +8,13 @@ public class EnemyDamage : MonoBehaviour
 {
     // Start is called before the first frame update
     public int hitPoints = 5;
-    public float knockForce = 100f;
     private bool canBeHit = true; 
-    public UnityEvent OnBegin, OnDone;
     void OnTriggerEnter2D(Collider2D other){
         if(other.gameObject.CompareTag("Bullet") && canBeHit){
             hitPoints --;
             StartCoroutine(Count());
         } 
     }
-    /*void OnCollisionEnter2D(Collision2D other){
-        if(other.gameObject.CompareTag("Player")){
-            Debug.Log("COLLIDING PLAYER");
-            Rigidbody2D rb = other.gameObject.GetComponent<Rigidbody2D>();
-            Vector2 opposite = -other.relativeVelocity.normalized;
-            rb.AddForce(opposite * knockForce, ForceMode2D.Impulse);
-        }
-    }*/
     IEnumerator Count(){
         canBeHit = false;
         Renderer rend = GetComponent<Renderer>();
@@ -33,7 +23,6 @@ public class EnemyDamage : MonoBehaviour
         rend.material.color = new Color(rend.material.color.r, rend.material.color.g, rend.material.color.b, 1f);
         canBeHit = true; 
     }
-    // Update is called once per frame
     void Update()
     {
         if(hitPoints == 0){
