9a88a690603f49d064b726826d1a0ae8bfc85089
josecarl0s1 <72897182+josecarl0s1@users.noreply.github.com>
2023-11-04 04:29:13 -0600

    Enemy Is Trigger

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -2,31 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class EnemyDamage : MonoBehaviour
 {
     // Start is called before the first frame update
     public int hitPoints = 5;
+    public float knockForce = 100f;
     private bool canBeHit = true; 
-    
+    public UnityEvent OnBegin, OnDone;
     void OnTriggerEnter2D(Collider2D other){
-        Debug.Log("ENTER COLLISION");
         if(other.gameObject.CompareTag("Bullet") && canBeHit){
-            Debug.Log("ENTER INSIDE BULLET");
             hitPoints --;
             StartCoroutine(Count());
-        }
+        } 
     }
+    /*void OnCollisionEnter2D(Collision2D other){
+        if(other.gameObject.CompareTag("Player")){
+            Debug.Log("COLLIDING PLAYER");
+            Rigidbody2D rb = other.gameObject.GetComponent<Rigidbody2D>();
+            Vector2 opposite = -other.relativeVelocity.normalized;
+            rb.AddForce(opposite * knockForce, ForceMode2D.Impulse);
+        }
+    }*/
     IEnumerator Count(){
-        Debug.Log("ENTER IENUM");
         canBeHit = false;
+        Renderer rend = GetComponent<Renderer>();
+        rend.material.color = new Color(rend.material.color.r, rend.material.color.g, rend.material.color.b, 0.3f);
         yield return new WaitForSeconds(0.5f);
+        rend.material.color = new Color(rend.material.color.r, rend.material.color.g, rend.material.color.b, 1f);
         canBeHit = true; 
     }
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("HIT POINTS" + hitPoints);
         if(hitPoints == 0){
             Destroy(this.gameObject);
         }
