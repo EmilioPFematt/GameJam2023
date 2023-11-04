3d9db9d882f40ebc0645f7f2061842bf3ac71d86
josecarl0s1 <72897182+josecarl0s1@users.noreply.github.com>
2023-11-04 04:55:41 -0600

    Player Takes Damage

--- Assets/Scripts/PlayerDamage.cs
+++ Assets/Scripts/PlayerDamage.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerDamage : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    public int hitPoints = 5;
+    private bool canBeHit = true; 
+    void OnTriggerStay2D(Collider2D other){
+        if(other.gameObject.CompareTag("Enemy") && canBeHit){
+            hitPoints --;
+            StartCoroutine(Count());
+        } 
     }
-
-    // Update is called once per frame
+    IEnumerator Count(){
+        canBeHit = false;
+        Renderer rend = this.transform.GetChild(0).GetComponent<Renderer>();
+        rend.material.color = new Color(rend.material.color.r, rend.material.color.g, rend.material.color.b, 0.3f);
+        yield return new WaitForSeconds(0.5f);
+        rend.material.color = new Color(rend.material.color.r, rend.material.color.g, rend.material.color.b, 1f);
+        canBeHit = true; 
+    }
     void Update()
     {
-        
+        if(hitPoints == 0){
+            Debug.Log("PLAYER IS DEAD");
+        }
     }
 }
