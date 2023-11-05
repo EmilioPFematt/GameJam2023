2caa8a0f2d6bdd682948277b5a6953b74eeebd71
josecarl0s1 <72897182+josecarl0s1@users.noreply.github.com>
2023-11-05 11:22:07 -0600

    buggyBoss

--- Assets/Scripts/ErrorMessageScript.cs
+++ Assets/Scripts/ErrorMessageScript.cs
@@ -7,12 +7,18 @@ public class ErrorMessageScript : MonoBehaviour
     public GameObject foreground;
     public GameObject rightWall;
     public GameObject bossAreanPrefab;
+    public GameObject background;
+    Camera camera;
+    void Awake(){
+        camera = Camera.main;
+    }
     void OnTriggerEnter2D(Collider2D other){
         if(other.gameObject.CompareTag("Bullet")){
             foreground.SetActive(false);
             rightWall.SetActive(false);
+            background.SetActive(false);
             Transform playerPosition =  GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-            GameObject bossArena = Instantiate(bossAreanPrefab, new Vector3(playerPosition.position.x, 0.25f, playerPosition.position.z), playerPosition.rotation);
+            GameObject bossArena = Instantiate(bossAreanPrefab, camera.ViewportToWorldPoint(new Vector2(0.5f, 0.5f)), Quaternion.identity);
             Destroy(this.gameObject);
         }
     }
