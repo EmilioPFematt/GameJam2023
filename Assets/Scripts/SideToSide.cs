6618990770a56af3da93f218f8fe761926264c97
josecarl0s1 <72897182+josecarl0s1@users.noreply.github.com>
2023-11-05 08:44:28 -0600

    FixedBat

--- Assets/Scripts/SideToSide.cs
+++ Assets/Scripts/SideToSide.cs
@@ -4,23 +4,24 @@ using UnityEngine;
 
 public class SideToSide : MonoBehaviour
 {
-    private float timeBeforeChange = 0.0f; 
+    private float timeBeforeChange = 0.0f;
     public float timeToMove = 0.5f;
     public float speed = 2.0f;
     private float newX;
-    private bool movingRight = false; 
+    private bool movingRight = true;
 
-    // Update is called once per frame
     void Update()
     {
-        if(timeBeforeChange > 0){
-                newX = transform.position.x + (movingRight ? speed * Time.deltaTime : -speed * Time.deltaTime);
-                timeBeforeChange -= Time.deltaTime;
-            } else{
-                timeBeforeChange  = timeToMove;
-                movingRight = !movingRight;
-            }
-            // Update the object's position
-            transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+        if (timeBeforeChange <= 0)
+        {
+            timeBeforeChange = timeToMove;
+            movingRight = !movingRight;
+        }
+
+        float moveDistance = speed * Time.deltaTime;
+        newX = transform.position.x + (movingRight ? moveDistance : -moveDistance);
+        timeBeforeChange -= Time.deltaTime;
+
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
     }
 }
